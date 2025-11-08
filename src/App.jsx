6ee1a1571ca9c7227ba7faa62596c6b5f88974ab
alasaneeth <alasaneeth22@gmail.com>
2025-11-08 15:00:35 +0400

    React Application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DiabetesPredictionApp from './components/DiabetesPredictionApp'
+
+const App = () => {
+  return (
+    <div>
+      <DiabetesPredictionApp/>
+    </div>
+  )
+}
+
+export default App
